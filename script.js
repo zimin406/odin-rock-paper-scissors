1d754521dd79feddb197c942f81e4ccd632e45f1
Gwak Jimin <zimin406@yonsei.ac.kr>
2023-08-04 00:27:58 +0900

    Add required functions to script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+function getComputerChoice () {
+    const choice= Math.floor(Math.random() * 3);
+    switch(choice) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        default:
+            return "scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Tie! You and Computer both selected ${playerSelection}.`;
+    }
+    else {
+        if (
+            (playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "rock")
+        ) {
+            return `You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+        }
+        else {
+            return `You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+        }
+    }
+}
+
+function capitalize (str) {
+    const firstChar = str.charAt(0)
+    return str.replace(firstChar, firstChar.toUpperCase());
+}
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        const playerSelection = prompt(`Rock, Paper, Scissors! ${i + 1}th Round`).toLowerCase(); 
+        console.log(playRound(playerSelection, getComputerChoice()));
+    }
+}
